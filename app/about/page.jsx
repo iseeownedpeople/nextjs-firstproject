60aa4cba632e02d04eab3ad4ddae6019916cdb0f
ESTEBAN FACUNDO <113944973+facundoesteban9@users.noreply.github.com>
2023-10-11 03:17:36 -0300

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,9 +1,8 @@
 'use client';
-import React from "react"
-import { useRouter } from "next/navigation"
+import React from "react" 
 
 function page() {
-    const router = useRouter()
+    
     return (
         <section>
             <h1>About</h1>
@@ -11,10 +10,7 @@ function page() {
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error provident molestias unde ut accusamus cupiditate deleniti esse, eos cum rem sed eveniet, aperiam exercitationem quam perferendis! Nulla, tempora, modi possimus unde provident officia velit, facilis consequuntur est aperiam tenetur aliquid commodi mollitia id aliquam similique hic laborum distinctio perspiciatis iure?
             </p>
             <button className='bg-sky-400 px-3 py-2 rounded-md'
-                onClick={() => {
-                    //podemos ejecutar logica previamente antes del push *componente cliente
-                    router.push('/')
-                }}> 
+                > 
                 Click
             </button>
         </section>
